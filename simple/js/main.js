692ddf2fccb4220431a3159c47b3fc948daf7d07
enlian <frejaenlian@gmail.com>
2024-10-13 01:02:09 +0900

    fix bug

---
+++ simple/js/main.js
@@ -0,0 +1,30 @@
+;(function () {
+	
+	'use strict';
+
+	var isiPad = function(){
+		return (navigator.platform.indexOf("iPad") != -1);
+	};
+
+	var isiPhone = function(){
+	    return (
+			(navigator.platform.indexOf("iPhone") != -1) || 
+			(navigator.platform.indexOf("iPod") != -1)
+	    );
+	};
+
+	var parallax = function() {
+		$(window).stellar({
+			horizontalScrolling: false,
+			hideDistantElements: false, 
+			responsive: true
+
+		});
+	};
+
+	$(function(){
+		parallax();
+	});
+
+
+}());
