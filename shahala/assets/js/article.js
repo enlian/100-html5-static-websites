48fa8ab9801cfcc986531fb7ba6a36f02c494100
enlian <frejaenlian@gmail.com>
2024-10-12 13:44:10 +0900

    first commit

---
+++ shahala/assets/js/article.js
@@ -0,0 +1,92 @@
+jQuery(document).ready(function () {
+
+
+
+
+    jQuery(".owl-carousel4").owlCarousel(
+        {
+            loop: true,
+            center: true,
+            margin: 0,
+            responsiveClass: true,
+            nav: false,
+            responsive: {
+                0: {
+                    items: 1,
+
+                },
+                600: {
+                    items: 1,
+
+                },
+                1000: {
+                    items: 1,
+
+                    loop: true
+                }
+            }
+        }
+    );
+
+    jQuery(".owl-carousel5").owlCarousel(
+        {
+            loop: true,
+            center: true,
+            margin: 0,
+            responsiveClass: true,
+            nav: false,
+            responsive: {
+                0: {
+                    items: 1,
+
+                },
+                600: {
+                    items: 1,
+
+                },
+                1000: {
+                    items: 1,
+
+                    loop: true
+                }
+            }
+        }
+    );
+});
+
+function myFunction(x) {
+    x.classList.toggle("change");
+}
+
+
+
+jQuery(".link-img").click(function () {
+    var link = jQuery(this).attr("data-link");
+
+    jQuery("#screen").attr("src", link)
+});
+
+
+
+var count = 0;
+jQuery("#toggle-search").click(function () {
+    count++;
+
+    var isEven = function (someNumber) {
+        return (someNumber % 2 === 0) ? true : false;
+    };
+
+    if (isEven(count) === false) {
+
+        jQuery("#nav-search").slideDown();
+        jQuery("#toggle-search").attr("src", "assets/images/close.png");
+
+    }
+
+    else if (isEven(count) === true) {
+
+        jQuery("#nav-search").slideUp();
+
+        jQuery("#toggle-search").attr("src", "assets/images/search-icon.png");
+    }
+});
